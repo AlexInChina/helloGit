b0edfa8981a3128e4235d4b042f77f90506c2665
qizhang <38871958+AlexInChina@users.noreply.github.com>
2021-07-03 18:14:32 +0800

    在线更新

--- maven_web/src/main/java/com/atguigu/HelloAtguigu.java
+++ maven_web/src/main/java/com/atguigu/HelloAtguigu.java
@@ -8,5 +8,6 @@ public class HelloAtguigu {
         System.out.println("hello git");
         System.out.println("创建分支");
         System.out.println("主干添加");
+        System.out.println("在线更新");
     }
 }
